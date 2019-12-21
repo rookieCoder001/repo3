fd9b00252e0e100b2d89712fcebf4d66fafb7e78
zhao1 <zhan1@qq.com>
2019-12-21 20:19:52 +0800

    提交了rookieCode项目

---
+++ rookieCode/Number3.java
@@ -0,0 +1,23 @@
+public class Number3{
+public static void main(String[] args){
+String str1 = "I am your father!";
+System.out.println(str1+88);//I am your father!88
+System.out.println(str1+"yes,you are ");//I am your father!yes,you are
+int a =5;
+int b =6;
+System.out.println(a+b);//11
+char zifu1= 'a';
+System.out.println(zifu1+88);//185
+short c = 99;
+System.out.println(c--);//99
+System.out.println(--c);//97
+byte d = 53;
+byte e = 49;
+int result = ++d - e--;
+System.out.println(result);//5
+System.out.println(d++);//54
+System.out.println(--e);//47
+System.out.println(d);//55
+System.out.println(e);//47
+}
+}
---
+++ rookieCode/OperatorDemo.java
@@ -0,0 +1,8 @@
+public class OperatorDemo {
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int result  = a>b?a:b;
+        System.out.println( "两者之间的较大值是:"+result );
+    }
+}
