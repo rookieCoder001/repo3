f682a7ea6ae3048b478324ff204f0c414b94d6aa
zhao1 <zhan1@qq.com>
2019-12-21 20:20:52 +0800

    删除了rookieCode项目

--- rookieCode/OperatorDemo.java
+++
@@ -1,8 +0,0 @@
-public class OperatorDemo {
-    public static void main(String[] args) {
-    int a = 10;
-    int b = 20;
-    int result  = a>b?a:b;
-        System.out.println( "两者之间的较大值是:"+result );
-    }
-}
